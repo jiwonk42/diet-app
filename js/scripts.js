9f6a2175d5d6275fceb0929ff28937976fb40008
Minh Phuong and Shruti Priya <minh@>
2017-01-30 16:28:25 -0800

    add constructor and new object using input variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // business logic
-
-
-
+function choice(habit,goals,number){
+  this.habit = habit;
+  this.goals = goals;
+  this.number = number;
+}
 
 
 // user logic goes here
 $(document).ready(function(){
 $("form").submit(function(event){
   event.preventDefault();
-
+//Make input into variables
  var inputBreakfast = $("input:radio[name='breakfast']:selected").val();
  var inputLunch = $("input:radio[name='lunch']:selected").val();
  var inputDinner = $("input:radio[name='dinner']:selected").val();
@@ -20,6 +22,8 @@ $("form").submit(function(event){
  var inputEatLess = $("input:checkbox[name='eatLess']:selected").val();
  var inputWeight = $(".weightQn option:selected").val();
  var inputNumber= parseInt($("input#weightQn").val());
+
+ var userChoice = new choice([inputBreakfast,inputLunch,inputDinner,inputStatus,inputSavvy,inputFiber,inputCondition],[inputEatMore,inputEatLess],[inputWeight,inputNumber])
 
 
   });
