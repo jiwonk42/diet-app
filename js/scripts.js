8e1ceeb1ed7c9ea5ebefff6be456159c00045d13
Jiwon Kang and Tammy Dang <tammy@>
2017-01-31 09:26:15 -0800

    create a variable for inputted Exercise Type that validates checkbox values and a variable called totalExperience that calculates the total score based on user input (selected values)

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,21 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var inputLifestyle = $("input:radio[name=lifestyle]:checked").val();
-    var inputCurrentExercise = $("input:radio[name=currentExercise]:checked").val();
-    var inputPriorityExercise = $("input:radio[name=priorityExercise]:checked").val();
-    var inputWeeklyExercise = $("input:radio[name=weeklyExercise]:checked").val();
-    var inputExercisePriority = $("input:radio[name=exercisePriority]:checked").val();
-    var inputExerciseType = $("input:checkbox[name=exerciseType]:checked").val();
+    var inputLifestyle = parseInt($("input:radio[name=lifestyle]:checked").val());
+    var inputCurrentExercise = parseInt($("input:radio[name=currentExercise]:checked").val());
+    var inputPriorityExercise = parseInt($("input:radio[name=priorityExercise]:checked").val());
+    var inputWeeklyExercise = parseInt($("input:radio[name=weeklyExercise]:checked").val());
+    var inputExercisePriority = parseInt($("input:radio[name=exercisePriority]:checked").val());
+    var inputExerciseType = parseInt($("input:checkbox[name=exerciseType]:checked").val());
 
-    
+    var inputExerciseType = 0;
+    $("input:checkbox[name=exerciseType]:checked").each(function() {
+      inputExerciseType += parseInt($(this).val());
+    });
+
+    var totalExercise = inputLifestyle + inputCurrentExercise + inputPriorityExercise + inputWeeklyExercise + inputExercisePriority + inputExerciseType;
+
+    console.log(totalExercise);
+
   });
 });
