c8f4298eaa86cc27719c65982b2bd1660a1cdfde
Minh Phuong and Shruti Priya <shruti@>
2017-01-31 11:08:14 -0800

    add prototypes to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,92 @@
-//define global variables
+//Define global variables
 var score = 0;
-// business logic
+//Business logic
 //Make original constructor
-function choice(habit,goals,number){
+function Choice(habit,goals,number){
   this.habit = habit;
   this.goals = goals;
   this.number = number;
 }
 //Make prototype for radio question score
-choice.prototype.scoreHabit = function () {
-  for (var i = 0; i < habit.length; i++) {
-    habit[i]
-  }
+Choice.prototype.scoreHabit = function () {
+  score = 0;
+  for (var i = 0; i < this.habit.length; i++) {
+    if(this.habit[i] === "1") {
+      score+=1
+    } else if (this.habit[i] === "3") {
+      score+=3
+    } else if (this.habit[i] === "5") {
+      score+=5
+    } else if (this.habit[i] === "7") {
+      score+=5
+    } else if (this.habit[i] === "2") {
+      score+=2
+    } else if (this.habit[i] === "4") {
+      score+=4
+    } else {
+      score+=6
+    }
+  };
+  console.log(score);
 };
-
-
+//Make prototype for checkbox questions:
+Choice.prototype.scoreGoal = function () {
+  for (var i = 0; i < this.goals.length; i++) {
+    if(this.goals[i] === "9") {
+      score+=9
+    } else if (this.goals[i] === "7") {
+      score+=7
+    } else if (this.goals[i] === "5") {
+      score+=5
+    } else if (this.goals[i] === "3") {
+      score+=3
+    } else if (this.goals[i] === "1") {
+      score+=1
+    }
+  }
+console.log(score);
+};
+//Make prototype for input field
+Choice.prototype.scoreNumber= function () {
+  if (this.number[0] === "1") {
+    score -= (this.number[1]/2)
+  } else { score += (this.number[1]/2)}
+};
 // user logic goes here
 $(document).ready(function(){
 $("form").submit(function(event){
   event.preventDefault();
-//Make input into variables
- var inputBreakfast = $("input:radio[name='breakfast']:selected").val();
- var inputLunch = $("input:radio[name='lunch']:selected").val();
- var inputDinner = $("input:radio[name='dinner']:selected").val();
- var inputStatus = $("input:radio[name='status']:selected").val();
- var inputSavvy= $("input:radio[name='savvy']:selected").val();
- var inputFiber = $("input:radio[name='fiber']:selected").val();
+//Make radio input into variable
+ var inputBreakfast = $("input:radio[name=breakfast]:checked").val();
+ var inputLunch = $("input:radio[name=lunch]:checked").val();
+ var inputDinner = $("input:radio[name=dinner]:checked").val();
+ var inputStatus = $("input:radio[name=status]:checked").val();
+ var inputSavvy= $("input:radio[name=savvy]:checked").val();
+ var inputFiber = $("input:radio[name=fiber]:checked").val();
  var inputCondition = $(".conditionMed option:selected").val();
- var inputEatMore = $("input:checkbox[name='eatMore']:selected").val();
- var inputEatLess = $("input:checkbox[name='eatLess']:selected").val();
+// Push checkbox input into an array
+ var inputGoals = [];
+ $("input:checkbox[name=eatMore]:checked").each(function() {
+   inputGoals.push($(this).val());
+ });
+ $("input:checkbox[name=eatLess]:checked").each(function() {
+   inputGoals.push($(this).val());
+ });
+ console.log(inputGoals);
+ // Get dropdown menu input
  var inputWeight = $(".weightQn option:selected").val();
  var inputNumber= parseInt($("input#weightQn").val());
 
- var userChoice = new choice([inputBreakfast,inputLunch,inputDinner,inputStatus,inputSavvy,inputFiber,inputCondition],[inputEatMore,inputEatLess],[inputWeight,inputNumber])
+//Create new object using constructor
+ var userChoice = new Choice([inputBreakfast,inputLunch,inputDinner,inputStatus,inputSavvy,inputFiber,inputCondition],inputGoals,[inputWeight,inputNumber])
+console.log(userChoice);
 
-
+//Apply prototypes to new object
+userChoice.scoreHabit();
+console.log(score);
+userChoice.scoreGoal();
+console.log(score);
+userChoice.scoreNumber();
+console.log(score);
   });
 });
