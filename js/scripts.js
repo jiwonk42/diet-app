37c43d2c3692f1a5e08d22ed670f3069f4d49668
Jiwon Kang and Tammy Dang <tammy@>
2017-01-31 16:20:21 -0800

    create an array object called ExerciseTypes that determines the type of exercise based on user choice

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ function Exercise(totalScore) {
   this.result = "";
 }
 
+var ExerciseTypes = {
+  rehab : ["20 min stretch", "15 min foam roll stretch"],
+  low : ["20 min walk", "10 min jog"],
+  resistence : ["30 min weight lighting"],
+  activities : ["30 min biking", "60 min hiking", "60 min rock climbing"],
+  core : ["45 min pilates", "60 min yoga"],
+  high : ["30 min circut training", "15 min interval sprinting"]
+}
+
 Exercise.prototype.results = function() {
   if(this.totalScore >=19 && this.totalScore <= 27) {
     this.result = "high intensity";
@@ -28,15 +37,37 @@ $(document).ready(function() {
     var inputExerciseType = parseInt($("input:checkbox[name=exerciseType]:checked").val());
 
     var inputExerciseType = 0;
-    $("input:checkbox[name=exerciseType]:checked").each(function() {
-      inputExerciseType += parseInt($(this).val());
-    });
-
+    var exerciseTypeArray = [];
     var totalExercise = inputWeeklyExercise + inputExercisePriority + inputExerciseType;
-
     var scoreResult = new Exercise(totalExercise);
 
-    console.log(scoreResult.results());
+    $("input:checkbox[name=exerciseType]:checked").each(function() {
+      inputExerciseType += parseInt($(this).val());
+      var suggestedExercise = "";
+
+      if ($(this).val() === "1") { // Rehabilitation
+        suggestedExercise = ExerciseTypes.rehab;
+      } else if ($(this).val() === "2") { // Low Intensity Training
+        suggestedExercise = ExerciseTypes.low;
+      } else if ($(this).val() === "3") { // Resistence Training
+        suggestedExercise = ExerciseTypes.resistence;
+      } else if ($(this).val() === "4") { // Activities
+        suggestedExercise = ExerciseTypes.activities;
+      } else if ($(this).val() === "5") { // Core Strength
+        suggestedExercise = ExerciseTypes.core;
+      } else { // High Intensity Training
+        suggestedExercise = ExerciseTypes.high;
+      }
+      exerciseTypeArray.push(suggestedExercise);
+    });
+
+
+    // for(var i=0; i< exerciseTypeArray.length; i++) {
+    //   //if(exerciseTypeArray[i] === 21)
+    //   eachValue += parseInt();
+    // }
+
+
 
   });
 });
